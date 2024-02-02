5707043d32bbdaa57c5738e39294fe370b815e5e
YOUNESSE25 <younessehaouas@example.com>
2024-02-01 16:37:33 -0800

    binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
 	newBTN = malloc(sizeof(binary_tree_t));
 	if (newBTN == NULL)
 		return (NULL);
