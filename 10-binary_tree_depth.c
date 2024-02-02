add0a57ea6b1ed8c0ace7ca525ef9600c5cdd8f7
YOUNESSE25 <younessehaouas@example.com>
2024-02-01 18:08:56 -0800

    binary trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree->parent == NULL || tree == NULL)
 		return (0);
 	else
 		return (1 + binary_tree_depth(tree->parent));
